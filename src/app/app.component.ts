01673544e357e854dd8bb919c0cad7aba30601b0
Markus Gede <markusgede93@gmail.com>
2023-10-26 13:46:03 +0200

    edit images and started on slide show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'markusgede';
+export class AppComponent implements OnInit {
+  images = ['uniko.jpg', 'fom.jpg', 'postbank.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage & this.images.length;
+      this.showImage = false;
+
+      setTimeout(()=>{
+        this.showImage = true;
+      },10)
+    }, 8000)
+
+  }
 }
